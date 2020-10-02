9db143607ad2e84de4d57c31bcfb56ab25dee83a
Deepanshu Gupta <41968942+deepanshug4@users.noreply.github.com>
2020-10-02 12:12:31 +0530

    Update health_prog.py

--- health_prog.py
+++ health_prog.py
@@ -1,12 +1,20 @@
+#Import Statements
+
 from pygame import mixer
 from math import floor as f
 import datetime
 import time as t
-print("Welcome to Healthy Programmer!")
+
+name = input("Enter your Name").capitalize()
+print(f"{name}, Welcome to Healthy Programmer!")
 print("We will keep reminding you to do your basic tasks again and again.")
 
 
 def play(sound):
+    """
+    This function is used to play different sounds.
+    This function takes the name of the file as a string
+    """
     mixer.init()
     mixer.music.load(sound)
     mixer.music.set_volume(50)
@@ -14,10 +22,16 @@ def play(sound):
 
 
 def time():
+    """This function returns us the current time."""
     return t.time()
 
 
 def water():
+    """
+    This function is used to deal with all the work related to water drinking.
+    This plays the song.
+    Then, log the time of drinking water.
+    """
     play("water.mp3")
     a = input("Type Drank if you have drank the water").lower()
     while a != "drank":
@@ -25,12 +39,17 @@ def water():
     if a == "drank":
         mixer.music.stop()
     file = open("water_log.txt", "a")
-    wr = "Drank water at: " + str(datetime.datetime.now()) + "\n"
+    wr = f"{name} drank water at: {datetime.datetime.now()} \n"
     file.write(wr)
     file.close()
 
 
 def eye():
+    """
+    This function is used to deal with all the work related to eye exercise.
+    This plays the song.
+    Then, log the time of doing eye exercise.
+    """
     play("eyes.mp3")
     a = input("Type eye if you have done the exercise").lower()
     while a != "eye":
@@ -38,12 +57,17 @@ def eye():
     if a == "eye":
         mixer.music.stop()
     file = open("eye_log.txt", "a")
-    wr = "Done eye Exercise at: " + str(datetime.datetime.now()) + "\n"
+    wr = f"{name} did eye exercise at: {datetime.datetime.now()} \n"
     file.write(wr)
     file.close()
 
 
 def exercise():
+    """
+    This function is used to deal with all the work related to exercise.
+    This plays the song.
+    Then, log the time of doing exercise.
+    """
     play("exercise.mp3")
     a = input("Type done if you have done the exercise").lower()
     while a != "done":
@@ -51,20 +75,27 @@ def exercise():
     if a == "done":
         mixer.music.stop()
     file = open("exercise_log.txt", "a")
-    wr = "Done exercise at: " + str(datetime.datetime.now()) + "\n"
+    wr = f"{name} did exercise at: {datetime.datetime.now()} \n"
     file.write(wr)
     file.close()
 
 
+# Customizable Variables
+water_after = 1200  # 20 minutes
+eye_after = 1800  # 30 minutes
+ex_after = 2700  # 45 minutes
+
+# Non-customizable Variables
 start_time = time()
-water_after = 1200 #20 minutes
-eye_after = 1800 #30 minutes
-ex_after = 2700 #45 minutes
 water_time = start_time
 eye_time = start_time
 ex_time = start_time
 
 
+''' This while loop does all the work. 
+Now sometimes all the activities will need to execcute at the same time.
+So, we use different if statements that will execute the processes one by one.  
+'''
 while True:
     x = f(time() - water_time) > water_after
     y = f(time() - eye_time) > eye_after
